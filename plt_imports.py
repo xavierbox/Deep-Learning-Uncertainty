1cf0c261ff6116975973b57ace5d68457b50aae1
xavierbox <42065172+xavierbox@users.noreply.github.com>
2020-07-04 23:36:56 +0100

    Update plt_imports.py

--- plt_imports.py
+++ plt_imports.py
@@ -2,7 +2,5 @@ import seaborn as sb, matplotlib
 from matplotlib import pyplot as plt, rcParams, animation
 matplotlib.rc('animation', html='html5')
 
-
-
 from ipywidgets import interact, interactive, fixed, widgets
 matplotlib.rc('animation', html='html5')
