2bf86fe451d8804eb10e56cc9b14af405043251f
Xavier Garcia-Teijeiro <xavierbox@gmail.com>
2020-07-05 10:15:51 +0100

    cleaned repeated imports

--- code/keras_imports.py
+++ code/keras_imports.py
@@ -1,32 +1,20 @@
 import keras.models;
 from keras.models import Sequential, Model, model_from_json, load_model
-from keras.layers import Input, Dense, Dropout,Activation
-from keras import metrics, optimizers,initializers,regularizers,layers
-from keras.optimizers import SGD
-from keras.callbacks import ModelCheckpoint,EarlyStopping 
-from keras import backend as k
-from keras.layers.advanced_activations import ReLU,PReLU,LeakyReLU
 
-
-from keras.models import Sequential, Model, model_from_json, load_model
-from keras.layers import Input, Dense, Dropout,Activation
 from keras import metrics, optimizers,initializers,regularizers,layers
-from keras.optimizers import SGD
-from keras.callbacks import ModelCheckpoint,EarlyStopping 
-from keras import backend as k
+from keras.layers import Input, Dense, Dropout,Activation, Dense, Flatten
 from keras.layers.advanced_activations import ReLU,PReLU,LeakyReLU
- 
-from keras.layers import Dense
-from keras.layers import Flatten
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import Conv3D
-
-from keras.layers.pooling import MaxPooling2D
-from keras.layers.pooling import MaxPooling3D
-
+from keras.layers.pooling import MaxPooling2D,  MaxPooling3D
 from keras.layers import Concatenate, Dense, LSTM, Input, concatenate
 
+from keras.optimizers import SGD
+from keras.callbacks import ModelCheckpoint,EarlyStopping 
+
+from keras import backend as k
 from keras.utils.vis_utils import plot_model
+
 import pydot
 import pydotplus
 from keras.utils.vis_utils import model_to_dot
