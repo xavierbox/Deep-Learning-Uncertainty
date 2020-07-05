a6c0f8bc20614b17593aa136700ddfc6ca507630
xavierbox <42065172+xavierbox@users.noreply.github.com>
2020-07-05 10:04:16 +0100

    Delete plt_imports.py

--- plt_imports.py
+++
@@ -1,6 +0,0 @@
-import seaborn as sb, matplotlib
-from matplotlib import pyplot as plt, rcParams, animation
-matplotlib.rc('animation', html='html5')
-
-from ipywidgets import interact, interactive, fixed, widgets
-matplotlib.rc('animation', html='html5')
